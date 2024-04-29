a7fe8d247a1da41b766b552e622da55ccfe8d499
subin0113 <subin0113@estsoft.com>
2024-04-29 09:17:34 +0900

    init project

---
+++ src/main/kotlin/com/aplus/batch/AplusBatchApplication.kt
@@ -0,0 +1,11 @@
+package com.aplus.batch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AplusBatchApplication
+
+fun main(args: Array<String>) {
+    runApplication<AplusBatchApplication>(*args)
+}
---
+++ src/test/kotlin/com/aplus/batch/AplusBatchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.aplus.batch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AplusBatchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
